5950450d8efe1fbf153752f673919d39a0213ac2
hidakago <hidakago@gmail.com>
2019-10-19 13:59:52 +0900

    Add:ルーティングにdisappearancesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "top#index"
+  resources :disappearances
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
