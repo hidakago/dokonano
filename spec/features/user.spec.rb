4f60276ea8c86b0b5412d23b65c0bb01c78b26cf
hidakago <hidakago@gmail.com>
2019-10-21 18:19:12 +0900

    Add:ログイン画面作成とfeature specテスト実施

--- spec/features/user.spec.rb
+++ spec/features/user.spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature "ユーザー登録機能", type: :feature do
+  background do
+    User.create!(name: 'test6', email: 'test6@mail.com', password: '5suke5suke', password_confirmation: '5suke5suke')
+  end
+
+
   # scenario（itのalias）の中に、確認したい各項目のテストの処理を書きます。
   scenario "ユーザー登録失敗のテスト" do
     visit new_user_registration_path
@@ -18,7 +23,7 @@ RSpec.feature "ユーザー登録機能", type: :feature do
     visit new_user_registration_path
 
     find(".name").set("test5")
-    find(".email").set("test5@email.com")
+    find(".email").set("test5@mail.com")
     find(".password").set("5suke5suke")
     find(".password_confirmation").set("5suke5suke")
     find(".signup").click
@@ -30,15 +35,24 @@ RSpec.feature "ユーザー登録機能", type: :feature do
   scenario "ログイン失敗のテスト" do
     visit new_user_session_path
 
-    find(".name").set("")
     find(".email").set("")
     find(".password").set("")
-    find(".password_confirmation").set("")
-    find(".signup").click
+    find(".login").click
 
-    expect(page).to have_content "保存されませんでした"
+    expect(page).to have_content "メールアドレス もしくはパスワードが不正です。"
+    # save_and_open_page
   end
 
+  scenario "ログイン成功のテスト" do
+    visit new_user_session_path
+
+    find(".email").set("test6@mail.com")
+    find(".password").set("5suke5suke")
+    find(".login").click
+
+    expect(page).to have_content "ログインしました。"
+    save_and_open_page
+  end
   # scenario "タスク作成のテスト" do
   #
   # end
